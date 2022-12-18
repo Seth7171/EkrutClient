5194187459039cf49f5b7e84c33957850f765fc4
ravid <ravid@DESKTOP-1SCETBM>
2022-12-18 14:16:18 +0200

    added report screen

--- ClientServerGUI_FX/src/guiReportScreens/ReportMainScreenController.java
+++ ClientServerGUI_FX/src/guiReportScreens/ReportMainScreenController.java
@@ -1,5 +1,25 @@
 package guiReportScreens;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class ReportMainScreenController {
+private Stage stage;
+private Scene scene;
+private Parent root;
+
+public void ClickType
+
+
+
+
+
+
+
+
+
+
 
 }
+
