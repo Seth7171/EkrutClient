c8c285da52444ea1148f81c866b61050e4952c1f
NitsanMaman <nit.maman@gmail.com>
2022-12-20 22:08:53 +0200

    New order in progress added scrollpane of products

--- ClientServerGUI_FX/src/gui/OrderScreens/ProductCatalogScreenController.java
+++ ClientServerGUI_FX/src/gui/OrderScreens/ProductCatalogScreenController.java
@@ -1,15 +1,20 @@
 package gui.OrderScreens;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 import application.client.ChatClient;
 import application.client.ClientUI;
 import application.user.UserController;
 import common.connectivity.Message;
 import common.connectivity.MessageFromClient;
+import common.orders.Order;
 import common.orders.Product;
 import gui.UserScreens.UserMainScreenController;
 import javafx.collections.FXCollections;
@@ -18,14 +23,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -48,19 +56,21 @@ public class ProductCatalogScreenController implements Initializable {
     private Button checkOutButton;
     
     @FXML
+    private TabPane tabPane;
+    
+    @FXML
     private TilePane snacksPane;
 
     @FXML
     private TilePane drinksPane;
     
     @FXML
-    private ScrollPane scrollSnacksPane;
-
-    @FXML
-    private ScrollPane scrollDrinksPane;
+    private ScrollPane snacksScroll;
     
     @FXML
-    private TabPane tabPane;
+    private ScrollPane drinksScroll;
+    
+    Order order = new Order();
     
     ObservableList<String> quantityPicker =
             FXCollections.observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
@@ -69,20 +79,17 @@ public class ProductCatalogScreenController implements Initializable {
     double yoffset;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-    	tabPane.getStyleClass().add("tab-pane");
-        ArrayList<String> credentials = new ArrayList<String>();
-        credentials.add(UserController.getCurrentuser().getUsername());
-    	ClientUI.chat.accept(new Message(credentials, MessageFromClient.REQUEST_ALL_MACHINE_PRODUCTS));
-        System.out.println(ChatClient.productList);
+        ClientUI.chat.accept(new Message("123", MessageFromClient.REQUEST_ALL_MACHINE_PRODUCTS));
+        tabPane.getStyleClass().add("tab-pane");
         for (Product product : ChatClient.productList) {
            // if(!product.isCustomMade())
-            	snacksPane.getChildren().add(createProductTile(product));
+        	 snacksPane.getChildren().add(createProductTile(product));
            // else
-            //	drinksPane.getChildren().add(createProductTile(product));
+           //     tilePaneSingle.getChildren().add(createProductTile(product));
         }
 
-        scrollSnacksPane.setFitToWidth(true);
-        scrollDrinksPane.setFitToWidth(true);
+        snacksScroll.setFitToWidth(true);
+        drinksScroll.setFitToWidth(true);
     }
     
     @FXML
@@ -92,11 +99,10 @@ public class ProductCatalogScreenController implements Initializable {
     }
     
     private Node createProductTile(Product product) {
-
-
         HBox hBox = new HBox();
         VBox vBox = new VBox();
-
+        
+        //Image image =new Image("@../logo.png");
         ImageView iv = new ImageView();
         iv.setFitHeight(250.0);
         iv.setFitWidth(150.0);
@@ -120,43 +126,83 @@ public class ProductCatalogScreenController implements Initializable {
 
         comboBoxQuantity.getSelectionModel().selectFirst();
 
-       // ClientUI.productController.createProductImage(product);
-      //  iv.setImage(ClientUI.productController.getProductImages().get(product.getProductId()));
+        //Client.productController.createProductImage(product);
+        //iv.setImage(image);
 
         // Check which price to display to the customer
-        if(product.getPrice() != product.getPrice()-product.getDiscount()) {
+        if(product.getDiscount()!=0) {
             priceLabel.setStrikethrough(true);
-            newPrice.setText("Discount Price: " + product.getDiscount() + " \u20AA");
+            newPrice.setText("Discount Price: " + (product.getDiscount() - product.getDiscount()) + " \u20AA");
             newPrice.getStyleClass().add("new-price-label");
         }
 
-        //if(Client.userController.getLoggedInUser() != null)
-        //    if(Client.userController.getLoggedInUser().getUserType() == UserType.CUSTOMER && ((Customer) Client.userController.getLoggedInUser()).isBlocked())
-         //       vBox.getChildren().addAll(nameLabel, priceLabel, newPrice, new Label("Color: " + product.getDominantColor()), viewDetails);
-        //    else
-             //   vBox.getChildren().addAll(nameLabel, priceLabel, newPrice, new Label("Color: " + product.()), comboBoxQuantity, addBtn, viewDetails);
-        //else
-          //  vBox.getChildren().addAll(nameLabel, priceLabel, newPrice, new Label("Color: " + product.getDominantColor()), viewDetails);
+        /*if(Client.userController.getLoggedInUser() != null)
+            if(Client.userController.getLoggedInUser().getUserType() == UserType.CUSTOMER && ((Customer) Client.userController.getLoggedInUser()).isBlocked())
+                vBox.getChildren().addAll(nameLabel, priceLabel, newPrice, new Label("Color: " + product.getDominantColor()), viewDetails);
+            else
+                vBox.getChildren().addAll(nameLabel, priceLabel, newPrice, new Label("Color: " + product.getDominantColor()), comboBoxQuantity, addBtn, viewDetails);
+        else*/
+        vBox.getChildren().addAll(nameLabel, priceLabel, newPrice, new Label("",viewDetails));
         hBox.getChildren().addAll(iv, vBox);
         vBox.setSpacing(15);
-       // vBox.setId(String.valueOf(product.get));
+        vBox.setId(String.valueOf(product.getProductId()));
         iv.setTranslateY(50);
-        hBox.setPadding(new Insets(5, 30, 20, 70));
+        hBox.setPadding(new Insets(0, 0, 0, 0));
         vBox.setPadding(new Insets(50, 30, 20, 25));
 
-        /*addBtn.setOnAction(event -> {
+        addBtn.setOnAction(event -> {
             String valueOfQuantity = comboBoxQuantity.getValue();
-
-            Client.orderController.addToCart(new OrderProduct(product, Integer.valueOf(valueOfQuantity)));
-            MainDashboardController.createAlert(product.getName() + " was added to cart", Alert.SUCCESS, Duration.seconds(2), 135, 67);
-            MainDashboardController.refreshCartCounter();
+            addToCart(product, Integer.valueOf(valueOfQuantity));
+            System.out.println("");
+            //MainDashboardController.refreshCartCounter();
         });
         addBtn.setCursor(Cursor.HAND);
         viewDetails.setOnAction(event -> {
-            currentProduct = product;
-            MainDashboardController.setContentFromFXML("ViewProductDetailsPage.fxml");
+            JOptionPane.showMessageDialog(null, product.getDescription(), "InfoBox: " + product.getName(), JOptionPane.INFORMATION_MESSAGE);
         });
-        viewDetails.setCursor(Cursor.HAND);*/
+
+        viewDetails.setBorder(null);
+        viewDetails.setCursor(Cursor.HAND);
         return hBox;
     }
+    
+    @FXML
+    void goBack(MouseEvent event) {
+    	Parent root = null;
+    	try {
+			root = FXMLLoader.load(getClass().getResource("/gui/UserScreens/UserMainScreen.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        Stage primaryStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+
+        root.setOnMousePressed(event1 -> {
+            xoffset = event1.getSceneX();
+            yoffset = event1.getSceneY();
+        });
+
+        // event handler for when the mouse is pressed AND dragged to move the window
+        root.setOnMouseDragged(event1 -> {
+            primaryStage.setX(event1.getScreenX()-xoffset);
+            primaryStage.setY(event1.getScreenY()-yoffset);
+        });
+        primaryStage.setTitle("Client Editor");
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    	
+    }
+    
+
+    void addToCart(Product product, int quantity) {
+    	
+    }
 }
+
+
+
+
