002789e04f8dda2bc681e2aac50419e0446d9741
Seth7171 <113588609+Seth7171@users.noreply.github.com>
2022-12-24 17:37:02 +0200

    added delivery, checkout controllers + fxml's

--- Client/src/gui/OrderScreens/GrabOrderScreenController.java
+++ Client/src/gui/OrderScreens/GrabOrderScreenController.java
@@ -5,25 +5,43 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import gui.ScreenController;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
-public class GrabOrderScreenController  extends ScreenController implements Initializable{
+public class GrabOrderScreenController extends ScreenController implements Initializable{
 
+    @FXML
+    private TextField orderNumField;
+    
     @FXML
     private Button exitButton;
 
     @FXML
     private Button backButton;
+    
+    @FXML
+    private Button grabNow;
 
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+		orderNumField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent evt1) {
+                if (evt1.getCode().equals(KeyCode.ENTER)) {
+                	GrabOrder(evt1);
+                }
+            }
+        });
 		
 	}
 
@@ -34,6 +52,17 @@ public class GrabOrderScreenController  extends ScreenController implements Init
 	
     @FXML
     void goBack(MouseEvent event) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/gui/UserScreens/UserMainScreen.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        super.switchScreen(event, root);        
+    }
+    
+    @FXML
+    void GrabOrder(Event event) {
         Parent root = null;
         try {
             root = FXMLLoader.load(getClass().getResource("/gui/UserScreens/UserMainScreen.fxml"));
