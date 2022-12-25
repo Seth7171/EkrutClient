b28716bf7d53f23b1668a26dbd3fe032df164a71
ravid <ravid@DESKTOP-1SCETBM>
2022-12-25 15:22:58 +0200

    Begin BarChart At ordersReport

--- Client/src/gui/ReportScreens/OrdersReportScreenController.java
+++ Client/src/gui/ReportScreens/OrdersReportScreenController.java
@@ -10,6 +10,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
@@ -23,7 +27,14 @@ public class OrdersReportScreenController extends ScreenController implements In
 
 	 @FXML
 	    private Button exitApp;
-	    
+	 @FXML
+	    private BarChart<String,Integer> OrdersChart;
+
+	    @FXML
+	    private CategoryAxis x;
+
+	    @FXML
+	    private NumberAxis y;
     @FXML
     void ClickBackButton(MouseEvent event) {
     	Parent root = null;
@@ -45,6 +56,22 @@ public class OrdersReportScreenController extends ScreenController implements In
 		// TODO Auto-generated method stub
 		DateChooseLabel.setText("*Report is relevant to " + ChatClient.returnMonthChoose + "/" + ChatClient.returnYearChoose);
 		
+		//making the BarChart from data
+		XYChart.Series<String, Integer> ser1= new XYChart.Series<>();
+		ser1.setName("Haifa");
+		ser1.getData().add(new XYChart.Data<String, Integer>("HA01", 200));
+		ser1.getData().add(new XYChart.Data<String, Integer>("HA02", 140));
+		ser1.getData().add(new XYChart.Data<String, Integer>("HA05", 36));
+		ser1.getData().add(new XYChart.Data<String, Integer>("HA09", 32));
+		
+		XYChart.Series<String, Integer> ser2= new XYChart.Series<>();
+		ser2.setName("Tel-Aviv");
+		ser2.getData().add(new XYChart.Data<String, Integer>("TA01", 110));
+		ser2.getData().add(new XYChart.Data<String, Integer>("TA02", 100));
+		ser2.getData().add(new XYChart.Data<String, Integer>("TA08", 110));
+		ser2.getData().add(new XYChart.Data<String, Integer>("TA03", 100));
+		
+		OrdersChart.getData().addAll(ser1,ser2);
 	}
 
 }
