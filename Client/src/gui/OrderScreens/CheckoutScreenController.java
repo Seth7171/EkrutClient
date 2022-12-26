86bc6c24c571b634d8a2c7fb2bbae31504b2ff74
NitsanMaman <nit.maman@gmail.com>
2022-12-26 23:51:29 +0200

    almost finish product

--- Client/src/gui/OrderScreens/CheckoutScreenController.java
+++ Client/src/gui/OrderScreens/CheckoutScreenController.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.client.ChatClient;
 import gui.ScreenController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 
 public class CheckoutScreenController extends ScreenController implements Initializable{
@@ -20,10 +22,12 @@ public class CheckoutScreenController extends ScreenController implements Initia
     @FXML
     private Button backButton;
     
+    @FXML
+    private ListView<Object> myOrder = new ListView<Object>();
+    
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
+		myOrder.setItems(ChatClient.rememberMyCart.getItems());
 	}
 
 	@FXML
@@ -33,9 +37,10 @@ public class CheckoutScreenController extends ScreenController implements Initia
 	
     @FXML
     void goBack(MouseEvent event) {
+    	ChatClient.rememberMyCart.setItems(myOrder.getItems());
         Parent root = null;
         try {
-            root = FXMLLoader.load(getClass().getResource("/gui/OrderScreens/GrabOrderScreen.fxml"));
+            root = FXMLLoader.load(getClass().getResource("/gui/OrderScreens/ProductCatalogScreen.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
