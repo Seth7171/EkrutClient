7fea3971e9b07254f6583ac606f7e12a5e9f6f20
ravid <ravid@DESKTOP-1SCETBM>
2023-01-05 14:22:35 +0200

    creating AREA MANAGER

---
+++ Client/src/gui/AreaManagersScreens/AreaManagerScreenController.java
@@ -0,0 +1,88 @@
+package gui.AreaManagersScreens;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import application.client.ClientUI;
+import application.user.UserController;
+import common.connectivity.Message;
+import common.connectivity.MessageFromClient;
+import gui.ScreenController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class AreaManagerScreenController extends ScreenController implements Initializable {
+
+	  @FXML
+	    private Button exitButton;
+
+	    @FXML
+	    private Button logOutButton;
+    @FXML
+    private Button manageProductsButton;
+
+    @FXML
+    private Button manageUsersButton;
+
+    @FXML
+    private Button viewRefilOrdersButton;
+
+    @FXML
+    private Button viewReportsButton;
+
+    @FXML
+    private Text welcomeText;
+
+    @FXML
+    void openManageProductsScreen(MouseEvent event) {
+    	
+    }
+
+    @FXML
+    void openRefilOrdersScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void openUserManagementScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void openViewReportsScreen(MouseEvent event) {
+
+    }
+
+    @FXML
+    void exit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logOut(MouseEvent event) {
+    	 ArrayList<String> cred = new ArrayList<String>();
+         cred.add(UserController.getCurrentuser().getUsername());
+         ClientUI.chat.accept(new Message(cred, MessageFromClient.REQUEST_LOGOUT));
+
+         Parent root = null;
+         try {
+             root = FXMLLoader.load(getClass().getResource("/gui/UserScreens/LogInScreen.fxml"));
+         } catch (IOException e) {
+             throw new RuntimeException(e);
+         }
+         super.switchScreen(event, root);
+    }
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		welcomeText.setText("Welcome back " + UserController.getCurrentuser().getFirstname());
+	}
+
+}
