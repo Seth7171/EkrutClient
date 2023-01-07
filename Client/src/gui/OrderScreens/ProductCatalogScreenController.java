2c296c4adabe254ea6039f73b3bea1f4a6d0ec42
NitMa <NitMa@10.100.102.78>
2023-01-08 01:09:42 +0200

    a little bit of jessica in my life....

--- Client/src/gui/OrderScreens/ProductCatalogScreenController.java
+++ Client/src/gui/OrderScreens/ProductCatalogScreenController.java
@@ -24,6 +24,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
