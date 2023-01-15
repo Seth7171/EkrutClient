8a3e3cd8e56a98b6125a4baf5bb9610a12382fdb
NitsanMaman <nit.maman@gmail.com>
2023-01-15 18:40:43 +0200

    added some javadocs

--- Client/src/application/client/ClientUI.java
+++ Client/src/application/client/ClientUI.java
@@ -6,13 +6,28 @@ import javafx.stage.StageStyle;
 
 import gui.ConnectionScreens.ClientConnectorController;
 
+/**
+ * The ClientUI class is the main class for the client side of the application.
+ * It extends the javafx Application class and overrides the start method to
+ * create a ClientConnectorController and display it on the primary stage.
+ */
 public class ClientUI extends Application {
-	public static ClientController chat; //only one instance
+    /**
+     * The chat variable holds the only instance of the ClientController (Singleton).
+     */
+	public static ClientController chat;
 	public static void main( String args[] ) throws Exception
 	   { 
 		    launch(args);  
 	   } // end main
 	 
+    /**
+     * The start method is called when the application is launched. It creates
+     * a new instance of the ClientConnectorController and displays it on the
+     * primary stage. The primary stage's decorations are removed.
+     * 
+     * @param primaryStage The primary stage of the application.
+     */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		ClientConnectorController aFrame = new ClientConnectorController(); // create StudentFrame
