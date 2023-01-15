07c63e5adcbdf059bda9283de1672c2896adc5db
NitsanMaman <nit.maman@gmail.com>
2023-01-15 20:32:34 +0200

    application.client javadoc

--- Client/src/application/client/ClientUI.java
+++ Client/src/application/client/ClientUI.java
@@ -10,6 +10,7 @@ import gui.ConnectionScreens.ClientConnectorController;
  * The ClientUI class is the main class for the client side of the application.
  * It extends the javafx Application class and overrides the start method to
  * create a ClientConnectorController and display it on the primary stage.
+ * @author Lior & Ron
  */
 public class ClientUI extends Application {
     /**
--- Client/src/application/client/MessageHandler.java
+++ Client/src/application/client/MessageHandler.java
@@ -1,20 +1,52 @@
 package application.client;
 
+/**
+ * The MessageHandler class holds and manages the message 
+ * and data fields for the client side of the application.
+ * @author Lior
+ */
 public class MessageHandler {
+    /**
+     * The message variable holds the message that is to be sent or received by the client.
+     */
     private static String message = null;
+    /**
+     * The data variable holds any additional data that is to be sent or received along with the message.
+     */
     private static Object data = null;
 
+    /**
+     * Returns the current message.
+     * 
+     * @return The current message.
+     */
     public static String getMessage() {
         return message;
     }
+    
+    /**
+     * Sets the message to a new value.
+     * 
+     * @param message The new value for the message.
+     */
     public static void setMessage(String message) {
         MessageHandler.message = message;
     }
 
+    /**
+     * Returns the current data.
+     * 
+     * @return The current data.
+     */
     public static Object getData() {
         return data;
     }
 
+    /**
+     * Sets the data to a new value.
+     * 
+     * @param data The new value for the data.
+     */
     public static void setData(Object data) {
         MessageHandler.data = data;
     }
