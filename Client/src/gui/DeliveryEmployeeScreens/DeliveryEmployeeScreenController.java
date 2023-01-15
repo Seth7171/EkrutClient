4de5e02d67117d7a64b04e45b13ee90bc1c4073f
Seth7171 <113588609+Seth7171@users.noreply.github.com>
2023-01-15 22:01:23 +0200

    more javadocs

--- Client/src/gui/DeliveryEmployeeScreens/DeliveryEmployeeScreenController.java
+++ Client/src/gui/DeliveryEmployeeScreens/DeliveryEmployeeScreenController.java
@@ -19,6 +19,11 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
+/**
+ * DeliveryEmployeeScreenController class is responsible for controlling the GUI for delivery employees.
+ * It allows the delivery employee to log out, view their orders, and exit the program.
+ * It also allows to displays the user's orders.
+ */
 public class DeliveryEmployeeScreenController extends ScreenController implements Initializable{
     
     @FXML
@@ -55,6 +60,13 @@ public class DeliveryEmployeeScreenController extends ScreenController implement
     private Text phoneText;
 
 
+    /**
+     * initialize method is responsible for initializing the GUI elements when the program starts.
+     * It sets the welcome message, user status, user's full name, id, department, email, and phone number.
+     * 
+     * @param arg0 url
+     * @param arg1 ResourceBundle
+     */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		 welcomeBackText.setText("Welcome Back " + UserController.getCurrentuser().getFirstname());
@@ -66,11 +78,21 @@ public class DeliveryEmployeeScreenController extends ScreenController implement
 	     phoneText.setText(UserController.getCurrentuser().getPhonenumber());
 	}
 	
+	/**
+	 * Closes the program when the exit button is pressed.
+	 * 
+	 * @param event the MouseEvent that triggers the exit method
+	 */
 	@FXML
     void exit(MouseEvent event) {
 		super.closeProgram(event, true);
     }
 	 
+	/**
+	 * Logs the current user out, sends a logout request to the server and redirects to the login screen.
+	 * 
+	 * @param event the Event that triggers the logOut method
+	 */
 	@FXML
     void logOut(Event event) {
         ArrayList<String> credentials = new ArrayList<String>();
@@ -87,6 +109,11 @@ public class DeliveryEmployeeScreenController extends ScreenController implement
     }
 	
 
+	/**
+	 * Redirects to the DeliveriesScreen.fxml where the current user can view their orders.
+	 * 
+	 * @param event the MouseEvent that triggers the viewOrders method
+	 */
 	@FXML
     void viewOrders(MouseEvent event) {
         Parent root = null;
--- Client/src/gui/DeliveryEmployeeScreens/TableOrder.java
+++ Client/src/gui/DeliveryEmployeeScreens/TableOrder.java
@@ -4,8 +4,18 @@ package gui.DeliveryEmployeeScreens;
 import common.orders.Order;
 import javafx.scene.control.ChoiceBox;
 
+/**
+*  TableOrder is a class that extends Order and contains additional fields for displaying information in a table
+* @author Nitsan & Ron
+* @version 1.0
+*/
 public class TableOrder extends Order {
     private ChoiceBox<String> status_co;
+    
+    /**
+    * Constructs a TableOrder object with the same properties as the given Order object
+    * @param order the order that this TableOrder is based on
+    */
 	public TableOrder(Order order) {
 		super(order.getOrderID(), 
 				order.getOverallPrice(), 
@@ -21,10 +31,18 @@ public class TableOrder extends Order {
 				order.getAddress());
 	}
 
+    /**
+    * Returns the status_co choicebox
+    * @return status_co
+    */
 	public ChoiceBox<String> getStatus_co() {
 		return status_co;
 	}
 
+    /**
+    * Set the status_co choicebox
+    * @param status_co choicebox
+    */
 	public void setStatus_co(ChoiceBox<String> status_co) {
 		this.status_co = status_co;
 	}
