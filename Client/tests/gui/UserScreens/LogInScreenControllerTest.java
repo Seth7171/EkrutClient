8013339b728346854e23549b0772090e31ac17ef
audiblemaple <audiblemaple@gmail.com>
2023-01-20 12:51:47 +0200

    Tests

---
+++ Client/tests/gui/UserScreens/LogInScreenControllerTest.java
@@ -0,0 +1,19 @@
+package gui.UserScreens;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogInScreenControllerTest {
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @Test
+    void checkValidUser(){
+
+    }
+}
