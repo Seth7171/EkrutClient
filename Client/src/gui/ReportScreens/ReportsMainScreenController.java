0bf687cc43e5fe2d9b1b4d2bc32177d2452c4551
NitsanMaman <nit.maman@gmail.com>
2023-01-21 20:38:03 +0200

    Reports Tests

--- Client/src/gui/ReportScreens/ReportsMainScreenController.java
+++ Client/src/gui/ReportScreens/ReportsMainScreenController.java
@@ -32,22 +32,44 @@ public class ReportsMainScreenController extends ScreenController implements Ini
 	/** Flags for machine id and location combo boxes. 1 means open, 0 means close.*/
 	int MachineIDFlag=0; // MachineID ComboBox:  1=open|0=close
 	int LocationFlag=0; // Location ComboBox:  1=open|0=close
-	
+	Parent root = null;
+	ArrayList<String> monthYearMachine = new ArrayList<>();
+	public int getLocationFlag() {
+		return LocationFlag;
+	}
+	public int getMachineIDFlag() {
+		return MachineIDFlag;
+	}
 	@FXML
     private ComboBox<String> Location;
+	public String getLocation() {
+		return Location.getValue();
+	}
 	
 	@FXML
     private ComboBox<String> MachineID;
+	public String getMachineID() {
+		return MachineID.getValue();
+	}
 
     @FXML
     private ComboBox<String> Month;
-
+	public String getMonth() {
+		return Month.getValue();
+	}
+	
     @FXML
     private ComboBox<String> Type;
-
+	public String getType() {
+		return Type.getValue();
+	}
+	
     @FXML
     private ComboBox<String> YearComboBox;
-
+	public String getYearComboBox() {
+		return YearComboBox.getValue();
+	}
+	
     @FXML
     private Button backButton;
 
@@ -217,65 +239,50 @@ public class ReportsMainScreenController extends ScreenController implements Ini
      * Move to the next window depends on Report type: Inventory, Clients or Orders.
      * @param event the mouse event that triggered the method call
      */
+
 	@FXML
-    void clickShowReport(MouseEvent event) {
+    String clickShowReport(MouseEvent event) {
 		
 		// Check first for valid inputs
-		if (Month.getValue() == null) {
-        	super.alertHandler("You didn't choose Month!" , true);
-			return;
+		if (getMonth() == null) {
+        	alertHandler("You didn't choose Month!" , true);
+			return "You didn't choose Month!";
 		}
-		if (YearComboBox.getValue() == null) {
-			super.alertHandler("You didn't choose Year!" , true);
-			return;
+		if (getYearComboBox() == null) {
+			alertHandler("You didn't choose Year!" , true);
+			return "You didn't choose Year!";
 		}
-		if (Type.getValue() == null) {
-			super.alertHandler("You didn't choose Report Type!" , true);
-			return;
+		if (getType() == null) {
+			alertHandler("You didn't choose Report Type!" , true);
+			return "You didn't choose Report Type!";
 		}
-		if (Location.getValue() == null && LocationFlag==1) {
-			super.alertHandler("You didn't choose Location !" , true);
-			return;
+		if (getLocation() == null && getLocationFlag()==1) {
+			alertHandler("You didn't choose Location !" , true);
+			return "You didn't choose Location !";
 		}
-		if (MachineID.getValue() == null && MachineIDFlag==1) {
-			super.alertHandler("You didn't choose Machine ID!" , true);
-			return;
+		if (getMachineID() == null && getMachineIDFlag()==1) {
+			alertHandler("You didn't choose Machine ID!" , true);
+			return "You didn't choose Machine ID!";
 		}
 		
 		
 		
 		//Save data for next window
-		 ChatClient.returnMachineID=MachineID.getValue(); // save the machine id that has been choosing
-		 ChatClient.returnMonthChoose=Month.getValue(); // save the month that has been choosing
-		 ChatClient.returnYearChoose=YearComboBox.getValue(); // save the year that has been choosing
-		 ChatClient.returnLocationChoose=Location.getValue(); // save the machine location that has been choosing
+		saveChatClientData();
 		 
 		
 		 
 		 //switch screens//
-		 Parent root = null;
-		 ArrayList<String> monthYearMachine = new ArrayList<>();
-		 monthYearMachine.add(Month.getValue());
-		 monthYearMachine.add(YearComboBox.getValue());
+		 monthYearMachine.add(getMonth());
+		 monthYearMachine.add(getYearComboBox());
 		
 		 try {
-			 	switch (Type.getValue())
+			 	switch (getType())
 			 		{
 			            case "Inventory":
-			            	 monthYearMachine.add(MachineID.getValue());
-			            	 
-							// request report from server
-							ClientUI.chat.accept(new Message(monthYearMachine, MessageFromClient.REQUEST_MACHINE_MONTHLY_INVENTORY_REPORT));
-							//if no such report
-							if (MessageHandler.getMessage() != null && MessageHandler.getMessage().contains("Error")) {
-								MessageHandler.setMessage(null);
-								Alert a = new Alert(Alert.AlertType.INFORMATION, "No monthly reports were found for this machine.");
-								a.setTitle("Report not found");
-								a.show();
-								return;
-							}
-
-			                 root = FXMLLoader.load(getClass().getResource("InventoryReportScreen.fxml"));
+			            	  if (inventoryReportCase(monthYearMachine,root).equals("Report not found")) {
+			            		  return "Report not found";
+			            	  }
 			                  break;
 	
 			            case "Orders":
@@ -292,7 +299,7 @@ public class ReportsMainScreenController extends ScreenController implements Ini
 								Alert a = new Alert(Alert.AlertType.INFORMATION, "Sorry,No monthly report were found.");
 								a.setTitle("Report not found");
 								a.show();
-								return;
+								return "Report not found";
 			            	}
 							root = FXMLLoader.load(getClass().getResource("OrdersReportScreen.fxml"));
 								break;
@@ -306,19 +313,54 @@ public class ReportsMainScreenController extends ScreenController implements Ini
 								Alert a = new Alert(Alert.AlertType.INFORMATION, "Sorry,No monthly report were found.");
 								a.setTitle("Report not found");
 								a.show();
-								return;
+								return "Report not found";
 			            	}
 			                  root = FXMLLoader.load(getClass().getResource("ClientsReportScreen.fxml"));
 			                   break;
 	
 			                default:
 			                    System.out.println("Unknown!");
+			                    break;
 		            }
 		 	}
 		 catch (IOException exception){
 			 exception.printStackTrace();
 		 }
-		 super.switchScreen(event, root);
+		switchToReport(event,root);
+		return "done";
+	}
+
+	public void switchToReport(MouseEvent event, Parent root){
+		super.switchScreen(event, root);
+	}
+	
+	public String inventoryReportCase(ArrayList<String> monthYearMachine,Parent root) {
+   	 monthYearMachine.add(getMachineID());
+	 
+		// request report from server
+		ClientUI.chat.accept(new Message(monthYearMachine, MessageFromClient.REQUEST_MACHINE_MONTHLY_INVENTORY_REPORT));
+		//if no such report
+		if (MessageHandler.getMessage() != null && MessageHandler.getMessage().contains("Error")) {
+			MessageHandler.setMessage(null);
+			Alert a = new Alert(Alert.AlertType.INFORMATION, "No monthly reports were found for this machine.");
+			a.setTitle("Report not found");
+			a.show();
+			return "Report not found";
+		}
+        try {
+			root = FXMLLoader.load(getClass().getResource("InventoryReportScreen.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        return "Report was found";
+	}
+	
+	public void saveChatClientData() {
+		//Save data for next window
+		 ChatClient.returnMachineID=getMachineID(); // save the machine id that has been choosing
+		 ChatClient.returnMonthChoose=getMonth(); // save the month that has been choosing
+		 ChatClient.returnYearChoose=getYearComboBox(); // save the year that has been choosing
+		 ChatClient.returnLocationChoose=getLocation(); // save the machine location that has been choosing
 	}
 
 }
